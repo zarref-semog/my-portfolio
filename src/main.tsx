30b97dc73b0a7d59384b482af45bd08693e4bab5
Murilo Gomes Ferraz <murilo.ferraz@flex-am.com.br>
2024-09-13 15:13:51 -0400

    feat: configs material theme and css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
 import App from './App.tsx'
+import { ThemeProvider } from '@emotion/react'
+import theme from "./theme.ts"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
