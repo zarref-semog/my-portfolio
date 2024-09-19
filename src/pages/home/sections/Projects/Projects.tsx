850cadd1b3c3609919a8e69a50f57ab70332a65b
Murilo Ferraz <103087349+zarref-semog@users.noreply.github.com>
2024-09-19 07:10:20 -0400

    Update Projects.tsx

--- src/pages/home/sections/Projects/Projects.tsx
+++ src/pages/home/sections/Projects/Projects.tsx
@@ -22,11 +22,11 @@ const Projects = () => {
     { img: ShoppingCartProject, alt: "shopping-cart-project", title: "Shopping Cart", href: "https://www.github.com/zarref-semog/shopping-cart", description: "A React JS application that consumes a Mercado Livre API, and displays your products with a search function." },
     { img: PokemonApiProject, alt: "pokemon-api-project", title: "Pokemon", href: "https://www.github.com/zarref-semog/pokemon-api", description: "A React JS application that consumes a Pokemon API, and allows you to favorite them." },
     { img: KanbanProject, alt: "kanban-project", title: "Kanban", href: "https://github.com/zarref-semog/kanban", description: "A Vanilla JS application that allows you to manage tasks in a kanban board." },
+    { img: "", alt: "", title: "Tractian Challenge", href: "https://github.com/zarref-semog/tractian-challenge-flutter", description: "Flutter mobile application to visualize Asset Tree." },
     { img: "", alt: "", title: "Parking Control", href: "https://github.com/zarref-semog/parking-control", description: "API for parking manage using Spring Boot and PostgreSQL." },
     { img: "", alt: "", title: "Day Notes", href: "https://github.com/zarref-semog/day-notes", description: "API for manage daily notes using ExpressJS and MySQL." },
     { img: "", alt: "", title: "Bitcoin Price", href: "https://github.com/zarref-semog/preco-bitcoin", description: "Flutter mobile application that shows the bitcoin price." },
     { img: "", alt: "", title: "Localiza", href: "https://github.com/zarref-semog/localiza", description: "API for car rental using ExpressJS and MongoDB." },
-    { img: "", alt: "", title: "Tractian Challenge", href: "https://github.com/zarref-semog/tractian-challenge-flutter", description: "Flutter mobile application to visualize Asset Tree." },
     { img: "", alt: "", title: "Consultoria", href: "https://github.com/zarref-semog/consultoria", description: "Flutter mobile application for a fictional company." },
     { img: "", alt: "", title: "Jokenpo", href: "https://github.com/zarref-semog/jokenpo", description: "Flutter mobile application to play jokenpo." },
   ];
